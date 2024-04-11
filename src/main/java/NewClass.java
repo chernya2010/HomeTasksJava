9c7cf3d42e232834a219226592da879a8d7e5fd0
chernya <chernya2010@gmail.com>
2024-04-11 19:36:43 +0300

    Lesson_1

---
+++ src/main/java/NewClass.java
@@ -0,0 +1,11 @@
+package main.java;
+import java.io.Console;
+
+public class NewClass {
+    public static void main(String[] args) {
+        String name;
+        Console con = System.console();
+        name = con.readLine("Please, enter your name: ");
+        System.out.println("Welcome to Java, " + name);
+    }
+}
