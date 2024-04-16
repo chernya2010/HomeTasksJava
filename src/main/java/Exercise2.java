537074b1c5c507d5638411ba2db7ca41da00d73b
chernya <chernya2010@gmail.com>
2024-04-16 14:09:20 +0300

    Corrections_for_exercise_2

--- src/main/java/Exercise2.java
+++ src/main/java/Exercise2.java
@@ -4,11 +4,9 @@ public class Exercise2 {
     public static void main(String[] args) { 
         //Экранирование кавычек
         System.out.println("Проснувшись однажды утром после беспокойного сна, \"Грегор Замза\" обнаружил, что он у себя в постели превратился в страшное насекомое.");
-
         //Сложение строк
         String a = "Привет";
         String b = "Валера";
-        System.out.println(a + " " + b);
-        
+        System.out.println(a + " " + b); 
     }
 }
