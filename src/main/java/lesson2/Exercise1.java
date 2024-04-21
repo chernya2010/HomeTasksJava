333679e956711929ba40ac66a2357679f6cdde97
chernya <chernya2010@gmail.com>
2024-04-21 15:16:21 +0300

    les2_hw_new_directory

---
+++ src/main/java/lesson2/Exercise1.java
@@ -0,0 +1,16 @@
+package main.java.lesson2;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        boolean a = true;
+        byte b = 100;
+        short c = 20000;
+        int d = 100000;
+        long e = 1000000;
+        float f = 10.65f;
+        double g = 11.58d;
+        char h = 'B';
+        
+        System.out.println(a + "\n" + b + "\n" + c + "\n" + d + "\n" + e + "\n" + f + "\n" + g + "\n" + h);
+    }
+}
