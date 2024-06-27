42cd8460a92cb724c3dafebe877d72e4d320aeb6
chernya <chernya2010@gmail.com>
2024-06-27 19:02:50 +0300

    fast_commit_lesson9

--- src/main/java/lesson9/string/StringHw2.java
+++ src/main/java/lesson9/string/StringHw2.java
@@ -1,18 +1,24 @@
 package main.java.lesson9.string;
 
-import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 import java.util.stream.Stream;
 
 public class StringHw2 {
     /*Дана строка произвольной длины с произвольными словами.*/
     static String s = "Проснувшись однажды утром после беспокойного сна, Грегор Замза обнаружил, что он у себя в постели превратился в страшное насекомое.";
+
     /*Найти самое короткое слово в строке и вывести его на экран.
     Если таких слов несколько, то вывести последнее из них.*/
-    public static void shrtestWord(String s){
+    public static void shortestWord(String s){
         String[] splitedString = s.split(" ");
-        String shortest = Stream.of(splitedString).min(Comparator.comparing(String::length)).get();
+//        String shortest = Stream.of(splitedString).min(Comparator.comparing(String::length)).get();
+//        System.out.println(shortest);
+        String shortest = splitedString[0];
+        for(int i = 1; i < splitedString.length;i++){
+            if(splitedString[i].length() <= shortest.length()){
+                shortest = splitedString[i];
+            }
+        }
         System.out.println(shortest);
     }
 
@@ -20,7 +26,19 @@ public class StringHw2 {
     Если таких слов несколько, то вывести последнее из них.*/
     public static void longestWord(String s){
         String[] splitedString = s.split(" ");
-        String longest = Stream.of(splitedString).max(Comparator.comparing(String::length)).get();
+//        String longest = Stream.of(splitedString).max(Comparator.comparing(String::length)).get();
+//        System.out.println(longest);
+        String longest = splitedString[0];
+        for(int i = 1; i < splitedString.length;i++){
+            if(splitedString[i].length() >= longest.length()){
+                longest = splitedString[i];
+            }
+        }
         System.out.println(longest);
+    }
+
+    public static void main(String[] args) {
+        shortestWord(s);
+        longestWord(s);
     }
 }
